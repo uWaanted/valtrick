04aa6c2f1f27752d358814da99c879e5a2481c8e
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-12-05 23:30:25 +0000

    abc

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Companies from "./components/Companies";
+
+export const revalidate=0
+
 export default function Home() {
   return (
-    <h1> TESTE </h1>
+    <div className="bg-[#3d3d3d]">
+      <div className="max-w-[1440px] m-auto ">
+    <h1> TESTE </h1>np
+    <Companies />
+
+    </div>
+      </div>
   )
 }
